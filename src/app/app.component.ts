37040d6501507d5fc3072e5da7d3d1c6516c45c5
Simon <snsimonescobar@gmail.com>
2022-01-06 15:45:11 -0500

    haciendo componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from "./product.model";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+  names = [
+    "simon",
+    "pedro",
+    "maria",
+    "angel",
+    "virginia"
+  ];
+
+  products: Product[] = [
+    {
+      id: 1,
+      image: 'assets/images/camiseta.png',
+      title: "Camiseta",
+      price: 54000,
+      description: "bla bla bla bla bla bla"
+    },
+    {
+      id: 2,
+      image: 'assets/images/camiseta.png',
+      title: "Camiseta",
+      price: 54000,
+      description: "bla bla bla bla bla bla"
+    },
+    {
+      id: 3,
+      image: 'assets/images/hoodie.png',
+      title: "hoodie",
+      price: 120000,
+      description: "bla bla bla bla bla bla"
+    },
+    {
+      id: 4,
+      image: 'assets/images/camiseta.png',
+      title: "Camiseta",
+      price: 54000,
+      description: "bla bla bla bla bla bla"
+    },
+  ];
+
+  addItems() {
+    this.names.push('nuevo item');
+  }
+
+  removeItem(index: number) {
+    this.names.splice(index, 1)
+  }
 }
